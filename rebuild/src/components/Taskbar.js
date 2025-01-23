ed8f70f415a4d747c81261d962f5c60f94187404
Kipland Melton <kiplandmelton@gmail.com>
2025-01-22 19:48:22 -0600

    adding desktop and taskbar components, initializing data service

---
+++ rebuild/src/components/Taskbar.js
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+import { Taskbar, List } from '@react95/core';
+import DataContext from '../utils/context.js';
+import styled from 'styled-components';
+
+function Taskbar() {
+  return 0;
+}
