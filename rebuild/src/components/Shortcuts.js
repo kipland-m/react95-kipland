321ef22c86e5649d5e61fe416dce9ed713688a47
Kipland Melton <kiplandmelton@gmail.com>
2025-01-26 00:13:50 -0600

    adding terminal shortcut

--- rebuild/src/components/Shortcuts.js
+++ rebuild/src/components/Shortcuts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { WindowsExplorer, MediaCd } from '@react95/icons';
+import { WindowsExplorer, MediaCd, FlyingThroughSpace100 } from '@react95/icons';
 import startWebamp from '../utils/shortcut-handlers/webamp.js';  
 
 
@@ -18,14 +18,29 @@ const IconWrapper = styled.div`
 function Shortcuts({ openExplorer }) {
   return ( 
     <div>
+
+      <ShortcutWrapper onClick={ openExplorer }>
+        <IconWrapper>
+          <WindowsExplorer variant="32x32_4" />
+        </IconWrapper>
+          Explorer
+      </ShortcutWrapper>
+
+      <ShortcutWrapper onClick={ startWebamp }>
+        <IconWrapper>
+          <FlyingThroughSpace100 variant="32x32_4" />
+        </IconWrapper>
+          Terminal 
+      </ShortcutWrapper>
+
       <ShortcutWrapper onClick={ startWebamp }>
         <IconWrapper>
           <MediaCd variant="32x32_4" />
         </IconWrapper>
           Winamp
       </ShortcutWrapper>
-    </div>
 
+    </div>
   );
 }
 
